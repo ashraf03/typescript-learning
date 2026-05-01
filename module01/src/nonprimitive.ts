f00f4a68f59114f3ba3814add27831277b825985
ashraf03 <eng.ashrafuddin.lus@gmail.com>
2026-05-01 22:54:44 +0600

    Added nonprimitive data type

---
+++ module01/src/nonprimitive.ts
@@ -0,0 +1,17 @@
+// array, object
+// ts - tuple
+
+let bazarList: string[] = ['eggs', 'milk', 'apple']
+
+// bazarList.push(true)
+
+let mixedArr:(string | number)[] = ['eggs', 12, 'milk', 1, "sugar", 2]
+
+
+let coordinates:[number, number] = [20, 30];
+
+let couple:[string, string] = ['Husband', 'Wife']
+
+let mezbaNameAndRoll:[string, number] = ["Mezba", 79]
+
+let travel:[string, string, number] = ['Dhaka', 'chotogram', 3]
