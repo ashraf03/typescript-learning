576d0a36aa38316d0b1e37be0b4794b648d2efa0
ashraf03 <eng.ashrafuddin.lus@gmail.com>
2026-05-01 22:37:40 +0600

    Added primitive data type

---
+++ module01/src/primitive.ts
@@ -0,0 +1,18 @@
+// JS - TS
+// string, number, boolean, undefinded, null
+
+// TS: never, unknown, void
+
+let userName: string = "AShraf Uddin";
+
+let userId:number = 1213;
+
+userName.toLowerCase()
+
+let isAdmin:boolean = false;
+
+isAdmin = true;
+
+let x:undefined = undefined;
+
+
