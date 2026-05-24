a55f01b4ef26e1d3242f5f9c99a544c69b3fb474
ashraf03 <eng.ashrafuddin.lus@gmail.com>
2026-05-24 20:20:54 +0600

    Practice again primitive.ts

--- module01/src/nonprimitive.ts
+++ module01/src/nonprimitive.ts
@@ -1,38 +1,7 @@
 // array, object
 // ts - tuple
 
-let bazarList: string[] = ['eggs', 'milk', 'apple']
-
-// bazarList.push(true)
-
-let mixedArr:(string | number)[] = ['eggs', 12, 'milk', 1, "sugar", 2]
+let bazarList: string[] = ["eggs", "Milk", "Sugar"];
 
 
-let coordinates:[number, number] = [20, 30];
-
-let couple:[string, string] = ['Husband', 'Wife']
-
-let mezbaNameAndRoll:[string, number] = ["Mezba", 79]
-
-let travel:[string, string, number] = ['Dhaka', 'chotogram', 3]
-
-
-// reference type: object
-
-const user:{
-    // organization: "Programing hero"; //value => type : literal type
-    readonly organization: "Programing hero"; //access modifier
-    firstName:string;
-    middleName?:string; //optional type
-    lastName:string;
-    isMarried: boolean;
-} = {
-    organization: "Programing hero", 
-    firstName: 'AShraf',
-    lastName: 'Tanjil',
-    isMarried: true,
-}
-
-
-
-console.log(user);
+bazarList.push(true);
